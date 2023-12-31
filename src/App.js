4d94a7c975af4f7ea9e332510eef39d1a3c163f3
Praveen Suthar <praveensuthar1729@gmail.com>
2023-12-30 18:07:11 -0700

    added react router with useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hats" element={<HatsPage />} />
+        </Routes>
       </div>
     );
   }
