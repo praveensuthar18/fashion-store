df79f5860f7ff137f100c196a7ef8d01358750d7
Praveen Suthar <praveensuthar1729@gmail.com>
2024-01-01 10:30:25 -0700

    Created signin component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
         </Routes>
       </div>
     );
